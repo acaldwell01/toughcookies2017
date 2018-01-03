6495c67f96b47825d55b2065ce03743c55307a24
EdinaToughCookies <31804996+EdinaToughCookies@users.noreply.github.com>
2018-01-03 09:03:15 -0600

    added arm test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotArmTest.java
@@ -15,16 +15,24 @@ public class PushbotArmTest extends LinearOpMode {
 
     TCHardwarePushbot robot = new TCHardwarePushbot();
 
+    double power1 = 0;
+    double pos1 = 0;
+
 
     public void runOpMode() throws InterruptedException {
 
         robot.init(hardwareMap);
+        robot.arm1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        robot.arm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.arm2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.arm2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
 
         while (opModeIsActive()) {
 
-        robot.arm1.setPower(-gamepad2.left_stick_y);
+        robot.arm2.setPower(-gamepad2.left_stick_y * 0.05);
+        robot.arm1.setPower(-gamepad2.right_stick_y * 0.05);
 
         }
 
